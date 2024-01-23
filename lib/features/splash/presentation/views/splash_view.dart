869411b08cbaa3d649332214554f646f331d953a
6sigmacode <sigmacode95@gmail.com>
2024-01-23 13:16:27 +0200

    add splash view body

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_clean_arich/features/splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -5,6 +6,7 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const SplashViewBody();
+
   }
 }
