ebc765724788564f3aa003e330a9cbac4c7f60f5
6sigmacode <sigmacode95@gmail.com>
2024-01-23 15:01:15 +0200

    add text splash and theme

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -10,10 +10,11 @@ class SplashViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      //crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
 
         Image.asset(AssetsData.logo),
+        const Text("Free Books"),
       ],
     );
   }
