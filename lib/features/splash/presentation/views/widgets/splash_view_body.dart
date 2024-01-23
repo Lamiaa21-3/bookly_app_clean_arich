afbf144673ca95d77381f3ab97a9e34dc497e977
6sigmacode <sigmacode95@gmail.com>
2024-01-23 16:29:09 +0200

    add animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,9 +2,38 @@ import 'package:bookly_clean_arich/core/utiltes/asset_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animationSliding;
+
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    super.initState();
+    animationSliding = Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+    animationSliding.addListener(() {
+      setState(() {
+
+      });
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +41,14 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       //crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-
         Image.asset(AssetsData.logo),
-        const Text("Free Books"),
+        AnimatedBuilder(
+          builder: (context,_) {
+            return SlideTransition(
+              position: animationSliding,
+                child:  Text("Free Books"));
+          }, animation: animationSliding,
+        ),
       ],
     );
   }
