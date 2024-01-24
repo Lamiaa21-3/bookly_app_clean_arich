ad8dc1878f932e3dbe7d7cd5177862e7f7e885ec
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-01-24 12:52:31 +0200

    tessssssssssssst

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,4 +1,5 @@
 
+import 'package:bookly_clean_arich/core/utiltes/asset_data.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,6 +7,27 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+
+    );
+  }
+}
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(AssetsData.logo),
+        IconButton(onPressed: (){}, icon: const Icon(Icons.search),),
+      ],
+    );
+
   }
 }
