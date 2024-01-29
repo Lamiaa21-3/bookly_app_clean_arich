af41d22a07c9aff04ef0d762b3a62fbaad45dcea
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-01-29 10:38:21 +0200

    add name

--- lib/features/home/presentation/view/widgets/best_seller_list_view_ietm.dart
+++ lib/features/home/presentation/view/widgets/best_seller_list_view_ietm.dart
@@ -2,6 +2,8 @@ import 'package:bookly_clean_arich/core/utiltes/asset_data.dart';
 import 'package:bookly_clean_arich/core/utiltes/styles_data.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../constants.dart';
+
 class BestSellerListViewIetm extends StatelessWidget {
   const BestSellerListViewIetm({Key? key}) : super(key: key);
 
@@ -12,15 +14,25 @@ class BestSellerListViewIetm extends StatelessWidget {
       child: Row(
         children: [
           Image.asset(AssetsData.test),
-           Column(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                width : MediaQuery.sizeOf(context).width * 0.5,
-                  child: const Text(
-                "Harry Potter And The Goblet Of Fire",
-                maxLines: 2,
-                style: Styles.textStyle20,
-              )),
+                  width: MediaQuery.sizeOf(context).width * 0.5,
+                  child: Text(
+                    "Harry Potter And The Goblet Of Fire",
+                    maxLines: 2,
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGtSectrFine),
+                  ),
+              ),
+              const SizedBox(height: 10,),
+              const Text('J.K rawlling ',style: Styles.textStyle14,),
+              const Row(
+                children: [
+                  Text('19.9\$',style: Styles.textStyle20,),
+                ],
+              ),
             ],
           ),
         ],
