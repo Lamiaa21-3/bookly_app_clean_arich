49aebaac63484ff9fbe3ebdd3e076581127b9fcf
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-02-20 11:04:24 +0200

    register hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 
 import 'package:bookly_clean_arich/core/utiltes/app_routers.dart';
+import 'package:bookly_clean_arich/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'constants.dart';
 
 
 void main() {
   runApp(const BooklyApp());
+  Hive.registerAdapter(BookEntityAdapter());
+  Hive.openBox(kFeaturedBox);
 }
 
 class BooklyApp extends StatelessWidget {
